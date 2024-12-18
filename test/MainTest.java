c0017a554ee2035b173df1cba9f6943a210618af
LeoKuo-0223 <leo900223@gmail.com>
2024-12-18 16:42:32 +0800

    update gitignore and add heelo world test

---
+++ test/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
